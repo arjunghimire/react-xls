e9311afd278e8d21f83673d0f90078bdb55b9418
Arjun Ghimire <arjun.ghimire@wolfmatrix.com>
2018-11-14 10:43:46 +0545

    export table data to xls

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 import 'App.css';
 import Workbook from 'react-excel-workbook';
 
-const data1 = [
+const data = [
   {
     fullname: 'Arjun Ghimire',
     contact: 'Kathmandu',
     phone: '9840122530',
-    status: 'active'
+    status: 'active',
+    user_type: 'admin'
   },
   {
     fullname: 'Sunil Regmi',
     contact: 'Kathmandu',
     phone: '9813999347',
-    status: 'active'
+    status: 'active',
+    user_type: 'admin'
   }
 ]
 
@@ -22,11 +24,14 @@ class App extends Component {
     return (
       <div className="row text-center" style={{marginTop: '100px'}}>
         <Workbook filename="example.xlsx" element={<button className="btn btn-lg btn-primary">Try me!</button>}>
-          <Workbook.Sheet data={data1} name="Sheet A">
+          <Workbook.Sheet data={data} name="Sheet A">
+            <Workbook.Column label="WCLMS Fleet" value="WCLMS Fleet"/>
+            <Workbook.Column label="Users" value="Users"/>
             <Workbook.Column label="Fullname" value="fullname"/>
             <Workbook.Column label="Contact" value="contact"/>
             <Workbook.Column label="Phone" value="phone"/>
             <Workbook.Column label="Status" value="status"/>
+            <Workbook.Column label="User Type" value="user_type"/>
           </Workbook.Sheet>
         </Workbook>
       </div>
